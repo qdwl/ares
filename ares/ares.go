f09b6bb5457e8c692409b5d86e4dbeb8248a1add
qdwl <outshine@126.com>
2019-07-04 13:51:45 +0000

    the first commit

---
+++ ares/ares.go
@@ -0,0 +1,9 @@
+// package ares implements a signal server based on WebSocket
+package ares 
+
+import (
+    "golang.org/x/net/websocket"
+)
+
+
+
---
+++ ares/relayTable.go
@@ -0,0 +1,16 @@
+package ares
+
+type relay struct {
+    internalIp    string
+    externalIp    string
+    port          int
+    country       string
+    province      string
+    city          string
+    isp           string
+    capacity      int
+}
+
+func newRelay(iip string, eip string, port int, country string, province string, city string, isp city, capacity int) {
+    return 
+}
